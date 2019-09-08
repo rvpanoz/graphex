9880e4493ff6ba5dd2f59bf0b36a260e5620bffa
rvpanos <rvpanoz@gmail.com>
2019-09-08 22:03:05 +0300

    chore: setup babel and folders structure

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// server configuration
+
+const config = {
+    mongoUrl: "mongodb+srv://dbAdmin:z0ubr0!28@cluster0-diifo.mongodb.net/db_zero",
+    port: 3030,
+    secret: "mySecret",
+    cookie: { maxAge: 60000 }
+};
+
+export default config;
---
+++ src/server.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import express from "express";
+import session from "express-session";
+import cors from "cors";
+import bodyParser from "body-parser";
+import errorHandler from "errorhandler";
+import config from "./config";
+
+const isProduction = process.env.NODE_ENV === "production";
+const { cookie, port, secret, mongoUrl } = config;
+
+// create application
+const app = express();
+
+// express config defaults
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+app.use(
+    session({
+        secret,
+        cookie,
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// register errorHandler
+if (!isProduction) {
+    app.use(errorHandler());
+}
+
+// start database connection
+if (isProduction) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect(mongoUrl, {
+        useNewUrlParser: true
+    });
+
+    // log queries in the console
+    mongoose.set("debug", true);
+
+    // disable buffering - https://mongoosejs.com/docs/connections.html
+    mongoose.set("bufferCommands", false);
+}
+
+// db events handling
+const db = mongoose.connection;
+
+// handle connection error
+db.on("error", error => console.error(error));
+
+// connection is established
+db.once("open", () => {
+    // start the server
+    const server = app.listen(process.env.PORT || port, function () {
+        console.log("Listening on port " + server.address().port);
+    });
+});
