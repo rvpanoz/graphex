4c7f959098937ee21d124bccb63f12c73518b3ea
rvpanoz <rvpanoz@gmail.com>
2019-09-09 10:53:59 +0300

    fix: add dotenv

--- src/configs/index.js
+++
@@ -1,7 +0,0 @@
-import configDev from "./dev";
-import configProd from "./prod";
-
-const selectedConfig =
-  process.env.NODE_ENV === "production" ? configProd : configDev;
-
-export default selectedConfig;
--- src/configs/prod.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  mongoUrl:
-    "mongodb+srv://testAdmin:testPass@cluster0-diifo.mongodb.net/db_zero",
-  port: 3030,
-  secret: "mySecret",
-  cookie: { maxAge: 60000 }
-};
-
-export default config;
